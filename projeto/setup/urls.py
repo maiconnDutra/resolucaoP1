a10640469490e1da87d80a5cfdd63ffb5933bf03
maiconnDutra <unifacul0@gmail.com>
2025-04-17 20:04:00 -0300

    questao 2

--- projeto/setup/urls.py
+++ projeto/setup/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from todos.views import TodoListView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', TodoListView.as_view(), name = 'todo_list')
 ]
--- projeto/todos/views.py
+++ projeto/todos/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+from .models import Todo
+
 # Create your views here.
+class TodoListView(ListView):
+    model = Todo
+
+class TodoCreateView(ListView):
+    model = Todo
+    fields = ['title','deadline']
+    success_url = reverse_lazy('todo_list')
